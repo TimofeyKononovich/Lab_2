a5f102d833ead6aadcceca2be7aee12294d9b25a
Timofey <timofeykononovich7@gmail.com>
2020-12-06 14:48:01 +0300

    first commit

---
+++ Lab_2.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Lab_2 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
+}
